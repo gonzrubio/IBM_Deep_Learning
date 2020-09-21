68a7f86c827f37a1c6e305dd90439cb092ab9b58
Gonzalo Rubio <gonzrubio95@gmail.com>
2020-09-21 16:46:51 -0700

    Fix bug in Neural Network with Multiple Dimension Inpute notes. Use the training function from the lab notebook.

--- PyTorch/Notes/Neural_Networks_with_Multiple_Dimension_Input.py
+++ PyTorch/Notes/Neural_Networks_with_Multiple_Dimension_Input.py
@@ -7,59 +7,68 @@ Shallow Neural Networks
 import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
+import numpy as np
 
 class XOR_Data(Dataset):
+    
+    # Constructor
     def __init__(self, N_s=100):
         self.x = torch.zeros((N_s, 2))
         self.y = torch.zeros((N_s, 1))
         for i in range(N_s // 4):
-            self.x[i,:] = torch.tensor([0.0, 0.0])
-            self.y[i,0] = torch.tensor([1.0])
+            self.x[i, :] = torch.Tensor([0.0, 0.0]) 
+            self.y[i, 0] = torch.Tensor([0.0])
 
-            self.x[i + N_s // 4,:] = torch.tensor([0.0, 1.0])
-            self.y[i + N_s // 4,0] = torch.tensor([1.0])            
+            self.x[i + N_s // 4, :] = torch.Tensor([0.0, 1.0])
+            self.y[i + N_s // 4, 0] = torch.Tensor([1.0])
+    
+            self.x[i + N_s // 2, :] = torch.Tensor([1.0, 0.0])
+            self.y[i + N_s // 2, 0] = torch.Tensor([1.0])
+    
+            self.x[i + 3 * N_s // 4, :] = torch.Tensor([1.0, 1.0])
+            self.y[i + 3 * N_s // 4, 0] = torch.Tensor([0.0])
 
-            self.x[i + N_s // 2,:] = torch.tensor([1.0, 0.0])
-            self.y[i + N_s // 2,0] = torch.tensor([1.0])              
-
-            self.x[i + 3 * N_s // 4,:] = torch.tensor([1.0, 1.0])
-            self.y[i + 3 * N_s // 4,0] = torch.tensor([1.0]) 
-            
             self.x = self.x + 0.01 * torch.randn((N_s, 2))
         self.len = N_s
-        
-    def __getitem__(self, index):
-        return self.x[index], self.y[index]
+
+    # Getter
+    def __getitem__(self, index):    
+        return self.x[index],self.y[index]
     
+    # Get Length
     def __len__(self):
         return self.len
     
 class Net(nn.Module):
-    def __init__(self,D_in, H, D_out):
+    def __init__(self, D_in, H, D_out):
         super(Net, self).__init__()
         self.linear1 = nn.Linear(D_in, H)
         self.linear2 = nn.Linear(H, D_out)
-        
-    def forward(self,x):
-        x = torch.sigmoid(self.linear1(x))
+
+    def forward(self, x):
+        x = torch.sigmoid(self.linear1(x))  
         x = torch.sigmoid(self.linear2(x))
         return x
 
+def accuracy(model, data_set):
+    return np.mean(data_set.y.view(-1).numpy() == (model(data_set.x)[:, 0] > 0.5).numpy())
 
-def train(data_set,model,criterion,train_loader,optimizer,epochs=5):
-    cost = []
-    acc = []
+def train(data_set, model, criterion, train_loader, optimizer, epochs=5):
+    COST = []
+    ACC = []
     for epoch in range(epochs):
-        total = 0
-        for y,x in train_loader:
-            loss = criterion(model(x),y)
+        total=0
+        for x, y in train_loader:
+            optimizer.zero_grad()
+            yhat = model(x)
+            loss = criterion(yhat, y)
             optimizer.zero_grad()
             loss.backward()
-            optimizer.step()
-            total += loss.item()
-        acc.append(accuracy(model,data_set))
-        cost.append(total)
-    return cost   
+            optimizer.step()            
+            total+=loss.item() #cumulative loss 
+        ACC.append(accuracy(model, data_set))
+        COST.append(total)
+    return COST   
 
 
 model = Net(2,4,1) # 2 input dimensions and 4 neurons in the hidden layer
